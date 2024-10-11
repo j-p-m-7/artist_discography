0ddf4525eaf0e16ab22348f3036b5eb160882a9a
Jack Monger <john.monger10@gmail.com>
2024-10-11 16:55:22 -0400

    removed files

--- historical_data/ascii_historical/ascii_test_v2.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pywhatkit as kit
-
-# Path to your image
-image_path = "image.png"
-
-# Convert the image to ASCII and save it to a text file
-kit.image_to_ascii_art(image_path, "ascii_output")
